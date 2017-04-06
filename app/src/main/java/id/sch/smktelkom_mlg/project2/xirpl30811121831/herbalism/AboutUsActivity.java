f7727966df89846e74ce9055a8d918b44351ef69
Fajar Yudha Pratama <fajaryudha23@gmail.com>
2017-04-06 16:15:15 +0700

    About us

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl30811121831/herbalism/AboutUsActivity.java
@@ -0,0 +1,14 @@
+package id.sch.smktelkom_mlg.project2.xirpl30811121831.herbalism;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+    }
+}
