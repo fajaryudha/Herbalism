ec9abdd9df7bc600d284c0fa8f2dcde00293e13c
Fajar Yudha Pratama <fajaryudha23@gmail.com>
2017-04-06 19:13:25 +0700

    About us

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl30811121831/herbalism/AboutUsActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl30811121831/herbalism/AboutUsActivity.java
@@ -1,7 +1,14 @@
 package id.sch.smktelkom_mlg.project2.xirpl30811121831.herbalism;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -11,4 +18,18 @@ public class AboutUsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about_us);
 
     }
+    public void instagram1(View view) {
+        Uri uri = Uri.parse("http://instagram.com/_u/dianofika");
+        Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+        likeIng.setPackage("com.instagram.android");
+
+        try {
+            startActivity(likeIng);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://instagram.com/_u/dianofika")));
+        }
+    }
+
 }
