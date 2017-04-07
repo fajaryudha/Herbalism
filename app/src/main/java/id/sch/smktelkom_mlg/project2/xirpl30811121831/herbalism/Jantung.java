c7bd7c0baa3a325f7c44e4e977742c6d732efc25
Agnes-PC <fitria.mela9a17@gmail.com>
2017-04-07 22:11:49 +0700

    arrays data tambahan lagi

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl30811121831/herbalism/Jantung.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project2.xirpl30811121831.herbalism;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Jantung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jantung);
+    }
+}
